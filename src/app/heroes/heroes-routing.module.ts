dff41d9327bbda09ab2a4f66c5e94271bd0d9f26
potlitel <potlitel@gmail.com>
2023-02-11 08:25:23 -0500

    Fin de la sección 12 del curso.

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 
 const rutas: Routes = [
   {
     path: '',
+    component:HomeComponent,
     children: [
       {
         path:'listado',
